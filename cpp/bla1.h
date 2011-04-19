51406e596f769ae7d8300a756c5056ef981861b0
Andrew Yourtchenko <ayourtch@gmail.com>
2011-04-19 03:22:03 +0200

    Some more testcases

---
+++ cpp/bla1.h
@@ -0,0 +1,10 @@
+#define FOO(x) ((x)*(x))
+#define TWOARG(y,z) ((z)+(y))
+#define THREEARG(x,y,z) [TWOARG(x,y)/(z)]
+// The below should not work.
+#define LOOP(x) LOOP(x+x)
+
+THREEARG(XXX,YYY, ZZZ)
+TWOARG(FOO(TWOARG(y1,z1)),THREEARG(TWOARG(y2,FOO(z2)), three, divizor))
+FOO(TWOARG(THREEARG(FOO(foo),YYY,TWOARG(ZZZ,TTT)),second_arg))
+
