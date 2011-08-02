57b93bcc1e4e165c0033c2083f6984895b2d7f05
Andrew Yourtchenko <ayourtch@gmail.com>
2011-08-03 00:06:07 +0200

    profiling additions

--- serv.lua
+++ serv.lua
@@ -1,25 +1,50 @@
+bit = require "bit"
 local S = require "syscall"
 local ffi = require("ffi")
 local sokt = require "sokt"
 local ht = require "httparse"
+local prof = require "profiler"
+
+local profiler = nil
+-- local profile = true
 
 -- Eventloop test bench
 
 local HTTP_REPLY = [[HTTP/1.0 200 OK
 Content-Type: text/plain
-
-This is test
 ]]
 
 local in_loop = true
 
 local my_accept_cb = function(fds, s)
   local cb = {}
+  if not profiler and profile then
+    print("Start profiling")
+    profiler = newProfiler()
+    profiler:start()
+  end
   cb.read = function(fds, s, data, len)
     local d = ht.parse(S.string(data, len), nil)
-    s:send(HTTP_REPLY, #HTTP_REPLY)
-    s:send(d.method, #d.method)
-    s:send(d.uri, #d.uri)
+    local reply = {}
+    local headers = {}
+    local out = HTTP_REPLY
+    if true then
+      table.insert(headers, HTTP_REPLY)
+      table.insert(reply, d.method)
+      table.insert(reply, " ")
+      table.insert(reply, d.uri)
+      table.insert(reply, "\n")
+      for i,v in pairs(d.hdr) do
+	table.insert(reply, "'" .. i .. "'")
+	table.insert(reply, ":")
+	table.insert(reply, v)
+	table.insert(reply, "\n")
+      end
+      local content = table.concat(reply)
+      table.insert(headers, "Content-Length: " .. #content .. "\r\n" )
+      local out = table.concat(headers) .. "\r\n" .. content
+    end
+    s:send(out, #out)
     fds.close(s)
   end
   cb.close = function(fds, s)
@@ -33,13 +58,19 @@ local my_udp_cb = function(fds, i)
   cb.read = function(fds, s, data, len, sa, sport)
     print("Received UDP packet:", S.string(data, len))
     in_loop = false
+    if profiler then
+      profiler:stop()
+      local outfile = io.open( "profile.txt", "w+" )
+      profiler:report( outfile )
+      outfile:close()
+    end
     -- fds.sendto(i, data, len, sa, sa_len)
   end
   return cb
 end
 
-
-
+print("signal", S.SIGPIPE, S.SIG_IGN)
+S.signal(S.SIGPIPE, S.SIG_IGN)
 
 ss = sokt.sokt()
 -- ss.add_listener("stream", 12345)
@@ -55,6 +86,7 @@ while not ss.add_udp_listener(12345, my_udp_cb) do
 end
 
 print("Added listener, please run the test")
+
 
 while true and in_loop do
   local n = ss.poll(1000)
